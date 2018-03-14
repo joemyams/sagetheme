b35bf5eae9aeb0be2ab23bddbd173e8bfe8f357c
Joshua <joemyams00@gmail.com>
2018-03-14 08:38:56 +0800

    my perfect sage theme

---
+++ mysagetheme/resources/views/partials/page-header.blade.php
@@ -0,0 +1,55 @@
+<div class="page-header">
+  <h1 class="animated flash">{!! App::title() !!}</h1>
+</div>
+
+<nav>
+ <button class="hamburger hamburger--collapse" type="button">
+  <span class="hamburger-box">
+    <span class="hamburger-inner"></span>
+  </span>
+  <span class="hamburger-label">Menu</span>
+</button>
+
+  <div id="navigation">
+ 
+                    <ul class="nav navbar-nav navbar-center">
+                        <li>
+                            <a href="#pablo">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#pablo">
+                                About Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#pablo">
+                                Products
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#pablo">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+            		<ul class="nav navbar-nav navbar-right">
+    		            <li>
+    		                <a href="https://twitter.com/CreativeTim">
+    							<i class="fa fa-twitter"></i>
+    						</a>
+    		            </li>
+    		            <li>
+    		                <a href="https://www.facebook.com/CreativeTim">
+    							<i class="fa fa-facebook-square"></i>
+    						</a>
+    		            </li>
+    					<li>
+    		                <a href="https://www.instagram.com/CreativeTimOfficial">
+    							<i class="fa fa-instagram"></i>
+    						</a>
+    		            </li>
+            		</ul>
+              </div>
+</nav>
